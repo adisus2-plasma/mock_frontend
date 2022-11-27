2cd058cb5a2f274140a5b94571c9b9edf50904de
pattarapol <pattarapol.sangsunee@mail.kmutt.ac.th>
2022-11-27 17:19:50 +0700

    [ADD]Authen

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import router from './router'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0XKFNnyUMG0vgh43Jac2PJ2Q7__dZ3Xo",
+  authDomain: "vue-firebase-auth-28f72.firebaseapp.com",
+  projectId: "vue-firebase-auth-28f72",
+  storageBucket: "vue-firebase-auth-28f72.appspot.com",
+  messagingSenderId: "955551655616",
+  appId: "1:955551655616:web:5697adc89397fe5979af25"
+};
 
-const app = createApp(App)
+initializeApp(firebaseConfig);
+
+const app = createApp(App);
+
+app.use(router)
 
 app.config.globalProperties.axios = axios
 app.mount('#app')
